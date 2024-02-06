1a81a7c68690da5a140cef14fd5041b7522aa6bb
jai <sjai5803@gmail.com>
2024-02-06 23:05:16 +0530

    boolean values

---
+++ boolean_value.py
@@ -0,0 +1,5 @@
+# 1. What is Boolean value?
+# A Boolean value is either true or false. In Python, the two Boolean values are True and
+# False (the capitalization must be exactly asshown), and the Python type is bool.
+a = True
+print(type(a))
