dd2f2c04b92baaee81ccec9cee09eb180b305f6a
jai <sjai5803@gmail.com>
2024-02-07 22:04:05 +0530

    program3

--- program3.py
+++ program3.py
@@ -1,3 +1,12 @@
 #longest substring without repeating characters
 
 s = "abcabcbb"
+new_s = ""
+for i in s:
+    new_s += i
+    if i == new_s[-1]:
+        continue
+    
+    
+
+print(f"longest substring without repeating characters is {new_s} and its length is {len(new_s)}")
