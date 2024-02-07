5fb319a67855ecbb92ee466aa35e35445937f11f
jai <sjai5803@gmail.com>
2024-02-07 21:58:36 +0530

    program2

---
+++ program2.py
@@ -0,0 +1,11 @@
+#program to check if a number is positive, negative or zero
+
+n = int(input())
+
+if n < 0:
+    print("the number is negative")
+elif n == 0:
+    print("the number is zero")
+else:
+    print("the number is positive")
+
